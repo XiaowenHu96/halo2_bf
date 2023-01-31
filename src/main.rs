f55c292c7cd8622419edb770610e26312cbb8f55
XiaowenHu96 <xihu5895@uni.sydney.edu.au>
2023-01-31 16:32:09 +0800

    add p2 and p3

--- src/main.rs
+++ src/main.rs
@@ -45,33 +45,39 @@ struct Config {
 
 impl Config {
     fn configure(cs: &mut ConstraintSystem<Fq>) -> Self {
+        let zero = Expression::Constant(Fq::zero());
         let one = Expression::Constant(Fq::one());
         let two = Expression::Constant(Fq::from(2));
 
         let clk = cs.advice_column();
-        let s_p = cs.selector();
-        cs.create_gate("P0: clk increase one per step", |vc| {
-            let s = vc.query_selector(s_p);
-            let cur_clk = vc.query_advice(clk, Rotation::cur());
-            let next_clk = vc.query_advice(clk, Rotation::next());
-            vec![s * (next_clk - cur_clk - one.clone())]
-        });
-
+        let ci = cs.advice_column();
+        let ip = cs.advice_column();
+        let ni = cs.advice_column();
+        let mp = cs.advice_column();
         let mv = cs.advice_column();
         let mvi = cs.advice_column();
         let s_c = cs.selector();
+        let s_p = cs.selector();
+
+        cs.create_gate("P0: clk increase one per step", |vc| {
+            let s_p = vc.query_selector(s_p);
+            let cur_clk = vc.query_advice(clk, Rotation::cur());
+            let next_clk = vc.query_advice(clk, Rotation::next());
+            vec![s_p * (next_clk - cur_clk - one.clone())]
+        });
+
         cs.create_gate("C0: mv is 0 or mvi is the inverse of mv", |vc| {
-            let s = vc.query_selector(s_c);
+            let s_c = vc.query_selector(s_c);
             let mv = vc.query_advice(mv, Rotation::cur());
             let mvi = vc.query_advice(mvi, Rotation::cur());
-            vec![s * mv.clone() * (mv * mvi - one.clone())]
+            vec![s_c * mv.clone() * (mv * mvi - one.clone())]
         });
 
         cs.create_gate("C1: mvi is 0 or mvi is the inverse of mv", |vc| {
-            let s = vc.query_selector(s_c);
+            let s_c = vc.query_selector(s_c);
             let mv = vc.query_advice(mv, Rotation::cur());
             let mvi = vc.query_advice(mvi, Rotation::cur());
-            vec![s * mvi.clone() * (mv * mvi - one.clone())]
+            vec![s_c * mvi.clone() * (mv * mvi - one.clone())]
         });
 
         // A deselector for op evalutes to zero iff ci != op
@@ -85,10 +91,6 @@ impl Config {
             })
         };
 
-        let ci = cs.advice_column();
-        let ip = cs.advice_column();
-        let mp = cs.advice_column();
-        let ni = cs.advice_column();
         cs.create_gate("P_1: instruction mutates state(1) correctly ", |vc| {
             let ci = vc.query_advice(ci, Rotation::cur());
             let deselectors = OPCODES
@@ -100,8 +102,9 @@ impl Config {
             let cur_mv = vc.query_advice(mv, Rotation::cur());
             let cur_mvi = vc.query_advice(mvi, Rotation::cur());
             let cur_ni = vc.query_advice(ni, Rotation::cur());
-            let s = vc.query_selector(s_p);
-            // ADD SUB SHR SHL GETCHAR PUTCHAR share the same p1 condition
+            let s_p = vc.query_selector(s_p);
+            // ADD SUB SHR SHL GETCHAR PUTCHAR share the same p1 condition:
+            // ip increases by 1
             let expr1 = (deselectors[ADD].clone()
                 + deselectors[SUB].clone()
                 + deselectors[SHR].clone()
@@ -119,7 +122,62 @@ impl Config {
                 * ((cur_mv.clone() * cur_mvi.clone() - one.clone())
                     * (next_ip.clone() - cur_ip.clone() - two.clone())
                     + (cur_mv.clone() * (next_ip.clone() - cur_ni.clone())));
-            vec![s * (expr1 + expr_lb + expr_rb)]
+            vec![s_p * (expr1 + expr_lb + expr_rb)]
+        });
+
+        cs.create_gate("P_2: instruction mutates state(2) correctly", |vc| {
+            let ci = vc.query_advice(ci, Rotation::cur());
+            let deselectors = OPCODES
+                .iter()
+                .map(|op| create_deselector(ci.clone(), *op))
+                .collect::<Vec<_>>();
+            let s_p = vc.query_selector(s_p);
+            let cur_mp = vc.query_advice(mp, Rotation::cur());
+            let next_mp = vc.query_advice(mp, Rotation::next());
+            // ADD, SUB, LB, RB, GETCHAR, PUTCHAR share the same p2 condition:
+            // memory pointer stay at the same
+            let expr1 = (deselectors[ADD].clone()
+                + deselectors[SUB].clone()
+                + deselectors[LB].clone()
+                + deselectors[RB].clone()
+                + deselectors[GETCHAR].clone()
+                + deselectors[PUTCHAR].clone())
+                * (next_mp.clone() - cur_mp.clone());
+            // SHL: mp decreases by one
+            let expr_shl =
+                deselectors[SHL].clone() * (next_mp.clone() - cur_mp.clone() + one.clone());
+            // SHR: mp increases by one
+            let expr_shr =
+                deselectors[SHR].clone() * (next_mp.clone() - cur_mp.clone() - one.clone());
+            vec![s_p * (expr1 + expr_shl + expr_shr)]
+        });
+
+        cs.create_gate("P_3: instruction mutates state(3) correctly", |vc| {
+            let ci = vc.query_advice(ci, Rotation::cur());
+            let deselectors = OPCODES
+                .iter()
+                .map(|op| create_deselector(ci.clone(), *op))
+                .collect::<Vec<_>>();
+            let s_p = vc.query_selector(s_p);
+            let cur_mv = vc.query_advice(mv, Rotation::cur());
+            let next_mv = vc.query_advice(mv, Rotation::next());
+            // LB, RB, PUTCHAR share the same p3 condition:
+            // memory value stay at the same
+            let expr1 =
+                (deselectors[LB].clone() + deselectors[RB].clone() + deselectors[PUTCHAR].clone())
+                    * (next_mv.clone() - cur_mv.clone());
+            // ADD: mv increases by 1
+            let expr_add =
+                deselectors[ADD].clone() * (next_mv.clone() - cur_mv.clone() - one.clone());
+            // sub: mv decreases by 1
+            let expr_sub =
+                deselectors[SUB].clone() * (next_mv.clone() - cur_mv.clone() + one.clone());
+            // SHL, SHR, GETCHAR: always true (check elsewhere)
+            let expr2 = (deselectors[SHL].clone()
+                + deselectors[SHR].clone()
+                + deselectors[GETCHAR].clone())
+                * (zero.clone());
+            vec![s_p * (expr1 + expr2 + expr_add + expr_sub)]
         });
 
         Self {
@@ -194,7 +252,6 @@ impl Config {
                 Ok(())
             },
         )
-
     }
 }
 
@@ -223,7 +280,7 @@ impl Circuit<Fq> for MyCircuit<Fq> {
         config: Self::Config,
         mut layouter: impl Layouter<Fq>,
     ) -> Result<(), Error> {
-        config.load_table(layouter, &self.matrix);
+        config.load_table(layouter, &self.matrix)?;
         Ok(())
     }
 }
