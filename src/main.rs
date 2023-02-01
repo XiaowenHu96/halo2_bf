8857c72ea674f5b948e815e2186f74b7d362610c
XiaowenHu96 <xihu5895@uni.sydney.edu.au>
2023-02-01 17:11:03 +0800

    Add main

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,21 @@ use halo2_bf::main_config::MyCircuit;
 use halo2_bf::utils::DOMAIN;
 use halo2_proofs::dev::MockProver;
 use halo2_proofs::halo2curves::bn256::Fq;
+use std::io::Read;
 
-fn main() {
-    let program = code::compile(include_bytes!("../ckb-bf-zkvm/res/neptune_tutorial.bf").to_vec());
-    let mut vm = Interpreter::new();
-    vm.set_code(program);
-    vm.set_input(code::easygen("a"));
-    vm.run();
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args: Vec<String> = std::env::args().collect();
+    assert!(args.len() >= 3);
+    let mut f = std::fs::File::open(&args[1])?;
+    let k = args[2].parse::<u32>()?;
+    let mut c: Vec<u8> = Vec::new();
+    f.read_to_end(&mut c)?;
+    let mut i = Interpreter::new();
+    i.set_code(code::compile(c));
+    i.run();
 
-    let circuit = MyCircuit::<Fq, { DOMAIN }>::new(vm.matrix);
-    let prover = MockProver::run(8, &circuit, vec![]).unwrap();
+    let circuit = MyCircuit::<Fq, { DOMAIN }>::new(i.matrix);
+    let prover = MockProver::run(k, &circuit, vec![]).unwrap();
     prover.assert_satisfied();
+    Ok(())
 }
