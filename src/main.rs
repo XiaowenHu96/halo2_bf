d058562b55f48e6c285268e48c1d582162fd0c42
XiaowenHu96 <xihu5895@uni.sydney.edu.au>
2023-02-02 13:29:15 +0800

    Calculate k

--- src/main.rs
+++ src/main.rs
@@ -6,16 +6,24 @@ use halo2_proofs::dev::MockProver;
 use halo2_proofs::halo2curves::bn256::Fq;
 use std::io::Read;
 
+/**
+ * halo2_bf <path_to_program> 
+ */
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args: Vec<String> = std::env::args().collect();
-    assert!(args.len() >= 3);
+    assert!(args.len() >= 2);
     let mut f = std::fs::File::open(&args[1])?;
-    let k = args[2].parse::<u32>()?;
     let mut c: Vec<u8> = Vec::new();
     f.read_to_end(&mut c)?;
     let mut i = Interpreter::new();
     i.set_code(code::compile(c));
     i.run();
+    let k = i
+        .matrix
+        .instruction_matrix
+        .len()
+        .next_power_of_two()
+        .trailing_zeros();
 
     let circuit = MyCircuit::<Fq, { DOMAIN }>::new(i.matrix);
     let prover = MockProver::run(k, &circuit, vec![]).unwrap();
